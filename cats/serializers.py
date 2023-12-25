6cfd35557c5ebe242285849e1d3a4d1710e9ecd8
Denis Savvateev <densavvateev@yandex.ru>
2023-12-26 00:06:25 +0300

    Fine tuning.

--- cats/serializers.py
+++ cats/serializers.py
@@ -26,12 +26,20 @@ class AchievementSerializer(serializers.ModelSerializer):
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     color = serializers.ChoiceField(choices=CHOICES)
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
     age = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
-                  'age')
+        fields = ('id', 'name', 'color', 'birth_year',
+                  'achievements', 'owner', 'age')
+        read_only_fields = ('owner',)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Cat.objects.all(),
+                fields=('name', 'owner')
+            )
+        ]
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
@@ -44,8 +52,22 @@ class CatSerializer(serializers.ModelSerializer):
             achievements = validated_data.pop('achievements')
             cat = Cat.objects.create(**validated_data)
             for achievement in achievements:
-                current_achievement, status = Achievement.objects.get_or_create(
-                    **achievement)
+                (current_achievement,
+                 status) = Achievement.objects.get_or_create(
+                    **achievement
+                )
                 AchievementCat.objects.create(
                     achievement=current_achievement, cat=cat)
             return cat
+
+    def validate_birth_year(self, value):
+        year = dt.date.today().year
+        if not (year - 40 < value <= year):
+            raise serializers.ValidationError('Проверьте год рождения!')
+        return value
+
+    def validate(self, data):
+        if data['color'] == data['name']:
+            raise serializers.ValidationError(
+                'Имя не может совпадать с цветом!')
+        return data
